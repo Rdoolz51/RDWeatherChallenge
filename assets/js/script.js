3440e00ee013564cb133b2d1cc9e2b43b8e25baa
Ryan Dooley <Rdoolz51@aol.com>
2022-09-09 16:20:42 -0400

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+//OpenWeather API key.
+const apiKey = '0ae21afd402b86cc39119d34076f8b0';
+
+
+function openWeatherInput (city) {
+    if (city) {
+        return `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    }
+}
+
+
 let cityInput = document.getElementById('city');
 let cityHistory = document.querySelector('.cList');
 let searchBtn = document.getElementById('searchBtn');
